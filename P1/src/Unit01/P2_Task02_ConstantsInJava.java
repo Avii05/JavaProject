b80f8c39b61ff5a3df176b0a3ff60c138c8dbaaf
c <c@DESKTOP-3JUMNJK>
2022-03-27 11:50:38 +0530

    Commit

---
+++ P1/src/Unit01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,19 @@
+package Unit01;
+
+public class P2_Task02_ConstantsInJava {
+	static final double pi=3.14;
+
+	public static final double PRICE=234.84;
+
+	public static void main(String[] agrs){
+		final int a=20;
+			System.out.println("Value of Pi="+pi);
+			System.out.println("a="+a);
+			System.out.println("Price= Rs."+PRICE);
+	}
+}
+
+
+
+
+
